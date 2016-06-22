3a442cd608abd965eb1e2148341e150402e4eaff
Ricardo Stefani <ricardostefanisantos@gmail.com>
2016-06-22 00:51:03 -0300

    enable moviments

--- js/code.js
+++ js/code.js
@@ -1,7 +1,57 @@
 function start() {
 	$("#inicio").hide();
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class'anima3'></div>");
+
+
+	var jogo = {}
+
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+	}
+
+	jogo.pressionou = [];
+
+	$(document).keydown(function (e) {
+		jogo.pressionou[e.which] = true;
+	});
+
+	$(document).keyup(function (e) {
+		jogo.pressionou[e.which] = false;
+	});
+
+	jogo.timer = setInterval(loop, 30);
+
+	function loop() {
+		movefundo();
+		movejogador();
+	}
+
+	function movefundo() {
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position", esquerda-1);
+	}
+
+	function movejogador() {
+		if(jogo.pressionou[TECLA.W]){
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top", top-10);
+		}
+		
+		if(jogo.pressionou[TECLA.S]){
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top", top+10);
+		}
+
+		if(jogo.pressionou[TECLA.D]){
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top", top+10);
+		}
+
+
+	}
 }
