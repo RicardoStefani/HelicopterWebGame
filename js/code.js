2d13e87d63266ad1dc5ed296e113c776ef7e236d
Ricardo Stefani <ricardostefanisantos@gmail.com>
2016-06-22 23:11:59 -0300

    explosion

--- js/code.js
+++ js/code.js
@@ -35,6 +35,7 @@ function start() {
 		moveinimigo1();
 		moveinimigo2();
 		moveamigo();
+		colisao();
 	}
 
 	function movefundo() {
@@ -115,6 +116,36 @@ function start() {
 				podeAtirar = true;
 			}
 
+		}
+	}
+
+	function colisao() {
+		var colisao1 = ($("#jogador").collision("#inimigo1"));
+		if (colisao1.length > 0) {
+			inimigo1X = parseInt($("#inimigo1").css("left"));
+			inimigo1Y = parseInt($("#inimigo1").css("top"));
+			explosao1(inimigo1X, inimigo1Y);
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left", 694);
+			$("#inimigo1").css("top", posicaoY);
+		}
+	}
+
+	function explosao1(inimigo1X, inimigo1Y) {
+		// somExplosao.play();
+		$("#fundoGame").append("<div id='explosao1'></div>");
+		$("#explosao1").css("background-image", "url(imgs/explosao.png)");
+		var div = $("#explosao1");
+		div.css("top", inimigo1Y);
+		div.css("left", inimigo1X);
+		div.animate({width: 200, opacity: 0}, "slow");
+
+		var tempoExplosao = window.setInterval(removeExplosao, 1000);
+
+		function removeExplosao() {
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao = null;
 		}
 	}
 }
