c601efab1174a7e1e27f98df37683ffd82755136
Ricardo Stefani <ricardostefanisantos@gmail.com>
2016-06-24 00:25:40 -0300

    create difficulty

--- js/code.js
+++ js/code.js
@@ -155,6 +155,7 @@ function start() {
 		}
 
 		if (colisao3.length > 0) {
+			velocidade += 0.3;
 			pontos += 100;
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
